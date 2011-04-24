e16ea993e249b585d90e1825db1f85b95338846c
Michal Kuklis <michal.kuklis@gmail.com>
2011-04-24 03:22:59 -0400

    added test for compressor.compile

--- test/compressor.test.js
+++ test/compressor.test.js
@@ -1,5 +1,12 @@
-var assert = require('assert')
+var fs = require('fs')
+  , assert = require('assert')
   , Compressor = require('compressor');
+
+// default options
+var options = {
+  configPath: process.cwd() + "/test/assets/config.json",
+  outputPath: process.cwd() + "/test/assets/templates.json"
+};
 
 module.exports = {
   'test .compile missing config throws exception': function() {
@@ -8,9 +15,16 @@ module.exports = {
     assert.throws(compressor.compile, "Config is required."); 
   },
 
-  "test constructor with config": function() {
-    var options = {configPath: process.cwd() + "/test/assets/config.json"}
-      , comp = new Compressor(options);
+  "test option configPath": function() {
+    var comp = new Compressor(options);
     assert.equal(options.configPath, comp.options.configPath);
+  },
+
+  "test .compile": function() {
+    var comp = new Compressor(options)
+      , templates = comp.compile();
+    assert.equal(templates.tmp1, "<div><div>template 1test</div>test</div>");
+    assert.equal(templates.subfolder.tmp3, "<div><div>template 3test</div>test</div>");
+    
   }
 }
